5934773ec249c439b94cd037172263329beb1c65
marsha <marshaannon@gmail.com>
2020-07-21 21:53:06 -0400

    added basic code to render form to home page

---
+++ src/components/auth/Registration.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class registrations extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            password_confirmation: "",
+            registrationErrors: ""
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log("form submitted");
+    }
+
+    handleChange(event) {
+        console.log("handle change", event);
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <input 
+                        type="password" 
+                        name="password_confirmation" 
+                        placeholder="Password confirmation"
+                        value={this.state.password_confirmation}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default registrations;
