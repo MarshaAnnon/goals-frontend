05f0448b6b75cfe81d6d03b6273ec1c6634896f0
marsha <marshaannon@gmail.com>
2020-07-21 23:25:58 -0400

    added handleSuccessfulAuth( & if conditional lines 37,38

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -34,7 +34,9 @@ class Registration extends Component {
         },
         { withCredentials: true }
         ).then(response => {
-            console.log("registration resp", response);
+            if (response.data.status === "created") {
+                this.props.handleSuccessfulAuth(response.data);
+            }
         }).catch(error => {
             console.log("registration error", error);
         })
